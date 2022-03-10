dad426618ab54639d0a10a05929e01b61bb7bb22
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-10 14:48:56 +0530

    UC1-AnsentOrPresent

---
+++ WageProblems/Program.cs
@@ -0,0 +1,13 @@
+﻿//UC1
+
+int present = 1;
+Random random = new Random();
+int Employeeattendance = random.Next(0, 2);
+if (Employeeattendance == present)
+{
+    Console.WriteLine("Employee is Present");
+}
+else
+{
+    Console.WriteLine("Employee is absent");
+}
