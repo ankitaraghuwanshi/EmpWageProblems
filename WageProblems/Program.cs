c6e8df2f541e3cc454b66fb495798872c58a8488
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-10 16:00:57 +0530

    UC7-PublicToPrivate

--- WageProblems/Program.cs
+++ WageProblems/Program.cs
@@ -3,26 +3,45 @@
 
 namespace WageProblems
 {
-    class program
+    class EmployeeWageComputation
     {
 
+        public int emp_hr_per_rate = 20;//constant
+        public int num_of_working_days = 2;//constant
+        public int max_working_hour = 100;//constant
+        public int EmpWagePerHour = 20;
         public const int is_part_time = 1;//constant
         public const int is_full_time = 2;//constant
-        public const int emp_hr_per_rate = 20;//constant
-        public const int num_of_working_days = 100;//constant
-        public const int max_hr_month = 10;//constant
-        static void Main(string[] args)
+        public int EmpDailywage = 0;
+        public float TotalWage = 0;
+
+        public EmployeeWageComputation(int emp_hr_per_rate, int num_of_working_days, int max_working_hour, int EmpDailyWage)
+        {
+            this.emp_hr_per_rate = emp_hr_per_rate;
+            this.num_of_working_days = num_of_working_days;
+            this.max_working_hour = max_working_hour;
+            this.EmpWagePerHour = EmpDailyWage;
+        }
+        private int IsEmpPresent()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            return empcheck;
+        }
+
+        public void CalculateWage()
         {
             int emphr = 0;
+            int empwage = 0;
+            int DayNumber = 1;
             int totalemphr = 0;
             int totalworkingdays = 0;
+            int totalempeage = 0;
 
-            while (totalemphr <= max_hr_month && totalworkingdays < num_of_working_days)
+            while (DayNumber <= max_working_hour && totalworkingdays <= num_of_working_days)
             {
-                totalworkingdays++;
-                Random random = new Random();
-                int empcheck = random.Next(0, 3);
-                switch (empcheck)
+                switch (IsEmpPresent())
+
                 {
                     case is_part_time:
                         emphr = 4;
@@ -34,12 +53,22 @@ namespace WageProblems
                         emphr = 0;
                         break;
                 }
-                totalemphr += emphr;
-                Console.WriteLine("Day=" + totalworkingdays + "Emphr =" + emphr);
+                EmpDailywage = EmpWagePerHour + EmpWagePerHour;
+                TotalWage += EmpDailywage;
+                DayNumber++;
+                emphr += EmpWagePerHour;
+
             }
-            int totalempwage = (emp_hr_per_rate * emphr);
-            Console.WriteLine("Total_Emp_Wage:" + totalempwage);
+            Console.WriteLine("Total working days:" + (DayNumber - 1) + "\nTotal working hours :" + emphr + "\nTotal employee wage :" + EmpDailywage);
+
         }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to employee wage Computation ");
+            EmployeeWageComputation employeeWageComputation = new EmployeeWageComputation(20, 2, 10, 20);
+            employeeWageComputation.CalculateWage();
+        }
+
 
     }
 }
