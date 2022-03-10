c54b3b3612185c63480dff2dd59cb57afc5ea825
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-10 15:48:55 +0530

    UC5-WorkDayFixed

--- WageProblems/Program.cs
+++ WageProblems/Program.cs
@@ -1,7 +1,7 @@
-﻿//UC4
+﻿//UC5
 
 
-namespace wageproblems
+namespace WageProblems
 {
     class program
     {
@@ -9,28 +9,36 @@ namespace wageproblems
         public const int is_part_time = 1;//constant
         public const int is_full_time = 2;//constant
         public const int emp_hr_per_rate = 20;//constant
-
+        public const int num_of_working_days = 20;
         static void Main(string[] args)
         {
             int emphr = 0;
             int empwage = 0;
-            Random random = new Random();
-            int empcheck = random.Next(0, 3);
-            switch (empcheck)
+            int totalempwage = 0;
+
+            for (int day = 0; day < num_of_working_days; day++)
             {
-                case is_part_time:
-                    emphr = 4;
-                    break;
-                case is_full_time:
-                    emphr = 8;
-                    break;
-                default:
-                    emphr = 0;
-                    break;
+                Random random = new Random();
+                int empcheck = random.Next(0, 3);
+                switch (empcheck)
+                {
+                    case is_part_time:
+                        emphr = 4;
+                        break;
+                    case is_full_time:
+                        emphr = 8;
+                        break;
+                    default:
+                        emphr = 0;
+                        break;
+                }
+                empwage = (emp_hr_per_rate * emphr);
+                totalempwage += empwage;
+                Console.WriteLine("Daily employee wage:" + empwage);
             }
-            empwage = (emp_hr_per_rate - emp_hr_per_rate * emphr);
-            Console.WriteLine("Daily employee wage:" + empwage);
+            Console.WriteLine("Total_Emp_Wage:" + totalempwage);
         }
 
     }
 }
+
