349de05d4363f6e56a66deec5932cd6685c30957
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-10 15:26:22 +0530

    UC3-FullOrPartTime

--- WageProblems/Program.cs
+++ WageProblems/Program.cs
@@ -1,24 +1,34 @@
-﻿//UC2
+﻿//UC3
 
 
 int Present = 1;
-int Empperrate = 20;
-int EmpWage = 0;
-int Emphours = 0;
+int Emp_per_rate = 20;
+int Emphr = 0;
+int empwage = 0;
+int Fulltime = 1;
 
 Random random = new Random();
+Random random1 = new Random();
 int Employeeattendance = random.Next(0, 2);
+int Employeetime = random.Next(0, 2);
 
 if (Employeeattendance == Present)
 {
-    Console.WriteLine("Employee is present");
-    Emphours = 8;
+    if (Employeetime == Fulltime)
+    {
+        Console.WriteLine("Employee is Present");
+        Emphr = 8;
+    }
+    else
+    {
+        Console.WriteLine("Employee is Present");
+        Emphr = 4;
+    }
 }
 else
 {
-    Console.WriteLine("Employee is absent");
-    Emphours = 0;
+    Console.WriteLine("Employee is Absent");
+    Emphr = 0;
 }
-EmpWage = (Empperrate * Emphours);
-
-Console.WriteLine("Daily wage = " + EmpWage);
+empwage = (Emp_per_rate * Emphr);
+Console.WriteLine("Daily Wage" + empwage);
