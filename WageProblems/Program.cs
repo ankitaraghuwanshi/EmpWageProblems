cc9045479a415d75688072320a898a02f0d00651
ankitaraghuwanshi <bittanraghu@gmail.com>
2022-03-10 15:00:23 +0530

    UC2-DailyWage

--- WageProblems/Program.cs
+++ WageProblems/Program.cs
@@ -1,13 +1,21 @@
-﻿//UC1
+﻿//UC2
 
-int present = 1;
+int Present = 1;
+int Empperrate = 20;
+int EmpWage = 0;
+int Emphours = 0;
+
 Random random = new Random();
 int Employeeattendance = random.Next(0, 2);
-if (Employeeattendance == present)
+if (Employeeattendance == Present)
 {
-    Console.WriteLine("Employee is Present");
+    Console.WriteLine("Employee is present");
+    Emphours = 8;
 }
 else
 {
     Console.WriteLine("Employee is absent");
+    Emphours = 0;
 }
+EmpWage = (Empperrate * Emphours);
+Console.WriteLine("Daily wage" + EmpWage);
